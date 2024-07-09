aaaadd1f4e45e74bc5753f4dc64c82215ca79b13
IKBAL RAMDANI <ikbalramdani33@gmail.com>
2024-07-09 14:52:20 +0700

    create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleAccountSeeder::class,
+            ProvinceSeeder::class,
+            RegencySeeder::class,
+            DistrictSeeder::class,
+        ]);
     }
 }
