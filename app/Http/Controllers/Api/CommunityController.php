e8072b55a6b7a5e46eeb3056d8f5e14b72b0e03a
rifdoalam <rifdoalam@gmail.com>
2024-07-21 03:03:52 +0700

    refactor : refactor this code community

--- app/Http/Controllers/Api/CommunityController.php
+++ app/Http/Controllers/Api/CommunityController.php
@@ -11,18 +11,27 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use Illuminate\Support\Facades\Date;
 use App\Helper\Communities as HelperCommunity;
-
+use App\Services\AuthService;
+use App\Services\CommunityService;
+use App\Services\UserCommunityService;
 class CommunityController extends Controller
 {
+    protected $authService;
+    protected $communityService;
+    protected $usercomService;
+    public function __construct(AuthService $authService, CommunityService $communityService, UserCommunityService $usercomService){
+        $this->authService = $authService;
+        $this->communityService = $communityService;
+        $this->usercomService = $usercomService;
+    }
     public function create_community(Request $request){
         
-        $user = User::with('profile')->where('slug',$request->id)->first();
+        $user = $this->authService->userBySlug($request->slug);
         $rules = [
             'name'=>['required'],
             'description'=>['required'],
             'address'=>['required'],
         ];
-
         $validator = Validator::make($request->all(),$rules);
         if ($validator->fails()) {
             return response()->json([
@@ -30,41 +39,38 @@ class CommunityController extends Controller
                 'message' => $validator->messages()->first()
             ], 404);
         }
-
-        $invite_code = HelperCommunity::generateCode(8);
-        DB::beginTransaction();
         try {
-            $community = new Community;
-            $community->invitation_code = $invite_code;
-            $community->name = $request->name;
-            $community->image = "image.jpg";
-            $community->description = $request->description;
-            $community->address = $request->address;
-            $community->balance = 0;
-            $community->save();
-
-            $userCommunity = new UserCommunity;
-            $userCommunity->user_id = $user->id;
-            $userCommunity->inviter_id = $user->id;
-            $userCommunity->community_id = $community->id;
-            $userCommunity->role = 'manager';
-            $userCommunity->is_owner = 1;
-            $userCommunity->is_accept = 1;
-            $userCommunity->responded_at = Date::now();
-            $userCommunity->save();
-
-            DB::commit();
+            $invite_code = HelperCommunity::generateCode(8);
+            $data = [
+                "invitation_code" =>$invite_code,
+                "name" => $request->name,
+                "image" => "image.jpg",
+                "description" => $request->description,
+                "address" => $request->address,
+                "balance" => 0
+            ];
+            $create_community = $this->communityService->createDataCommunity($data);
+            $user_community = [
+                "user_id" => $user->id,
+                "inviter_id" => $user->id,
+                "community_id" => $create_community->id,
+                "role" => 'manager',
+                "is_owner" => 1,
+                "is_accept" => 1,
+                "responded_at" => Date::now()
+            ];
+            $create_usercom = $this->usercomService->createUserCommunity($user_community);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Success',
+                'data' => $create_usercom
+            ], 200);
         } catch (\Exception $e) {
             DB::rollback();
             return response()->json([
                 'status' => 'error',
-                'message' => 'internal_error'
+                'message' => $e
             ], 500);
         }
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Success'
-        ], 200);
-        
     }
 }
