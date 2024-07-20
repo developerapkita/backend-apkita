28f15fd2760b28f5be9e737f8a8aff20d8b8cff2
rifdoalam <rifdoalam@gmail.com>
2024-07-21 03:05:51 +0700

    feat : add new migration

---
+++ database/migrations/2024_07_20_191554_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('slug')->unique()->default(DB::raw('(UUID())'));
+            $table->integer('community_id');
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->text("rsvp")->nullable();
+            $table->timestamp('start_date')->nullable();
+            $table->timestamp('end_date')->nullable();
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
