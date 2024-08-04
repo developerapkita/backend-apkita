bceda8e8da75d073cedc2d747e3183501cbdbede
IKBAL RAMDANI <ikbalramdani33@gmail.com>
2024-08-04 21:30:13 +0700

    update get data event

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -6,8 +6,11 @@ class EventService{
     public function createEvent(array $data){
         return Event::create($data);
     }
-    public function getEventBySlug(string $slug){
-        return Event::where('slug', $slug)->first();
+    public function getEventByUser(string $id){
+        return Event::join('user_communities','user_communities.community_id','events.community_id')->where('user_communities.user_id', $id)->get();
+    }
+    public function getEventByGroup(string $group){
+        return Event::join('user_communities','user_communities.community_id','events.community_id')->where('user_communities.community_id', $group)->get();
     }
     public function updateEvent(string $slug, array $data){
         return Event::where('slug',$slug)->update($data);
