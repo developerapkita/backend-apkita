d3857ddb9db4b494378e558031938191872b63e2
IKBAL RAMDANI <ikbalramdani33@gmail.com>
2024-08-04 21:31:03 +0700

    add any endpoint api

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\ImportDataController;
 use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\PlaceController;
 use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\EventCommentController;
 use App\Http\Controllers\Api\ComplainController;
 
 /*
@@ -36,7 +37,7 @@ Route::get('/v1/data-regency/{provinceCode}', [PlaceController::class, 'getRegen
 Route::get('/v1/data-district/{regencyCode}', [PlaceController::class, 'getDistrictByRegencyCode']);
 
 
-Route::middleware(['auth:sanctum'])->prefix('/v1')->group(function () {
+Route::middleware(['auth:sanctum','rolecheck:member'])->prefix('/v1')->group(function () {
     //ROUTING FOR Auth
     Route::POST('/profile-complete/{slug}', [AuthController::class, 'profile_complete']);
     Route::POST('/set-pin/{id}', [AuthController::class, 'set_pin']);
@@ -46,26 +47,41 @@ Route::middleware(['auth:sanctum'])->prefix('/v1')->group(function () {
     //ROUTING FOR PROFILE
     Route::get('/profile/{slug}', [ProfileController::class, 'show']);
     Route::put('/profile/{slug}', [ProfileController::class, 'update']);
+    //Community
     Route::post('/community/create/{slug}', [CommunityController::class, 'create_community']);
     Route::get('/community/{id}', [CommunityController::class, 'check_community']);
+    Route::get('/community/show/{slug_user}', [CommunityController::class, 'show_community']);
     Route::POST('/community/{id}/{inviter}/{slug}', [CommunityController::class, 'join_community_qr']);
 
 
-    Route::middleware(['rolecheck:member'])->group(function () {
+    Route::middleware(['rolegroupcheck:member'])->group(function () {
+        //Complain
         Route::get("/complain/{slug}",[ComplainController::class, 'show']);
         Route::post("/complain",[ComplainController::class, 'create']);
-        Route::put("/complain/{slug}",[ComplainController::class, 'update']);
+        // Route::put("/complain/{slug}",[ComplainController::class, 'update']);
         Route::delete("/complain/{slug}",[ComplainController::class, 'delete']);
-    });
-    Route::middleware(['rolecheck:admin,manager'])->group(function () {
-        Route::post('/event', [EventController::class, 'create']);
+
         Route::get("/event/{slug}",[EventController::class, 'show']);
-        Route::put("/event/{slug}",[EventController::class, 'update']);
+        //Event Comment
+        Route::post('/event/create-comment/{id}/{slug}', [EventCommentController::class, 'create']);
+        Route::get('/event/show-comment/{slug}', [EventCommentController::class, 'show']);
+    });
+    Route::middleware(['rolegroupcheck:manager'])->group(function () {
+        //Event
+        Route::post('/event/create', [EventController::class, 'create']);
+        Route::get("/event/user/{slug}",[EventController::class, 'showByUser']);
+        Route::get("/event/group/{slug}",[EventController::class, 'showByGroup']);
+        Route::put("/event/update/{slug}",[EventController::class, 'update']);
         Route::delete("/event/{slug}",[EventController::class, 'destroy']);
-    
+        //Event Comment
+        Route::post('/event/create-comment/{id}/{slug}', [EventCommentController::class, 'create']);
+        Route::get('/event/show-comment/{slug}', [EventCommentController::class, 'show']);
+        //Complain
         Route::put("/complain/{slug}",[ComplainController::class, 'replyData']);
     });
+    Route::middleware(['rolegroupcheck:support'])->group(function () {
 
+    });
 });
 
 //Community
